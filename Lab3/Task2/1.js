bbe04cdeac295e4838b42c22813446f6e498f9ee
baxaa <onalbekovbakbergen7@gmail.com>
2023-02-14 18:48:39 +0600

    asdasd

--- Lab3/Task2/1.js
+++ Lab3/Task2/1.js
@@ -6,11 +6,16 @@ function push() {
     Li += `<li id = "lis1">
                 <input type = "checkbox" class = "check">
                 <p class = "tasks"> ${inputlist.value}</p>
-                <button class = "trash">
+                <button class = "trash" onclick = "del()" >
                     <img class = "image" src="http://atlas-content-cdn.pixelsquid.com/stock-images/trash-can-dustbin-ENAKKRB-600.jpg" >
                 </button>
-                <hr id = "hr">
+
             </li>`
     inputlist.value = null
     taskbox.innerHTML += Li
+}
+
+function del(){
+    delete items.pop();
+    taskbox.innerHTML;
 }
