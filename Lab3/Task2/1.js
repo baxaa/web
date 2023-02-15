36fdcbfa3d692845df66788e030ce17de93d062c
baxaa <onalbekovbakbergen7@gmail.com>
2023-02-15 19:36:37 +0600

    task2

--- Lab3/Task2/1.js
+++ Lab3/Task2/1.js
@@ -8,7 +8,7 @@ function push() {
     var arr = []
     Li += `<li id = "lis1">
                 <input type = "checkbox" class = "check" onclick = "cross(${a})">
-                <p class = "tasks" id = "${a}"> ${inputlist.value} </p>
+                <p class = "tasks" id = "ass${a}"> ${inputlist.value} </p>
                 <button class = "trash" onclick = "del(${a})" >
                     <img class = "image" src="http://atlas-content-cdn.pixelsquid.com/stock-images/trash-can-dustbin-ENAKKRB-600.jpg" >
                 </button>
@@ -45,14 +45,16 @@ function del(id) {
 function cross(id){
     for (var i = 0; i < items.length; i++) {
         if(items[i][1] == id && items[i][2] == false){
-            document.querySelector(".tasks").className = "crossline"
+            document.getElementById(`ass${id}`).classList.remove("tasks")
+            document.getElementById(`ass${id}`).classList.add("crossline")
             items[i][2] = true
-            console.log(items[i])
+            // console.log(items[i])
             break
         }else if (items[i][1] == id && items[i][2] == true){
             items[i][2] = false
-            document.querySelector(".crossline").className = "tasks"
-            console.log(items[i])
+            document.getElementById(`ass${id}`).classList.remove("crossline")
+            document.getElementById(`ass${id}`).classList.add("tasks")
+            // console.log(items[i])
             break
         }
     }
