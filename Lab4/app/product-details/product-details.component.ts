46fbe7b81f1a28cdbc2bcf2638395a1a83c0e452
baxaa <onalbekovbakbergen7@gmail.com>
2023-02-22 16:52:21 +0600

    finish third part of project

--- Lab4/app/product-details/product-details.component.ts
+++ Lab4/app/product-details/product-details.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+  // constructor(private route: ActivatedRoute) { }
   ngOnInit() {
     // First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
